db77d347c373d3dc793b274aea1083a66c90892e
Arseniy Trushin <tass@thoth.travel>
2020-04-19 17:19:42 +0300

    input a name

--- web/welcome/views.py
+++ web/welcome/views.py
@@ -3,32 +3,17 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from django.template import loader
+
 
 def index(request):
-    result = '<h1>Welcome to our color bars project</h1>' \
-             '<svg width="400" height="110">' \
-             '<rect width="300" height="100" style="fill:rgb(130, 65, 0);stroke-width:0;stroke:rgb(0,0,0)" />' \
-             '</svg>' \
-             '<svg width="400" height="110">' \
-             '<rect width="300" height="100" style="fill:rgb(200, 200, 0);stroke-width:0;stroke:rgb(0,0,0)" />' \
-             '</svg>' \
-             '<svg width="400" height="110">' \
-             '<rect width="300" height="100" style="fill:rgb(200, 0, 0);stroke-width:0;stroke:rgb(0,0,0)" />' \
-             '</svg>' \
-             '<svg width="400" height="110">' \
-             '<rect width="300" height="100" style="fill:rgb(0, 130, 0);stroke-width:0;stroke:rgb(0,0,0)" />' \
-             '</svg>' \
-             '<svg width="400" height="110">' \
-             '<rect width="300" height="100" style="fill:rgb(0, 0, 0);stroke-width:0;stroke:rgb(0,0,0)" />' \
-             '</svg>'             \
-             '<svg width="400" height="110">' \
-             '<rect width="300" height="100" style="fill:rgb(130, 0, 70);stroke-width:0;stroke:rgb(0,0,0)" />' \
-             '</svg>' \
-             '<svg width="400" height="110">' \
-             '<rect width="300" height="100" style="fill:rgb(130, 130, 130);stroke-width:0;stroke:rgb(0,0,0)" />' \
-             '</svg>' \
-             '<svg width="400" height="110">' \
-             '<rect width="300" height="100" style="fill:rgb(0, 0, 130);stroke-width:0;stroke:rgb(0,0,0)" />' \
-             '</svg>'
-
-    return HttpResponse(result)
+    template = loader.get_template('welcome/index.html')
+    colors = ['(130, 65, 0)',
+              '(200, 200, 0)',
+              '(0, 130, 0)',
+              '(0, 0, 0)',
+              '(130, 0, 70)',
+              '(130, 130, 130)',
+              '(0, 0, 130)',
+              ]
+    return HttpResponse(template.render({'colors': colors}, request))
